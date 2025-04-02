d46c140f62b4064edf170de9238cf62fcf02bb5a
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-04-02 15:17:17 +0100

    chore: changed carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,31 +1,29 @@
 import { Component } from "solid-js";
-import {
-  TAS,
-  SolarFoundation,
-  SwitchElectric,
-  Arkreen,
-} from "~/assets/companies";
+// import {
+//   TAS,
+//   SolarFoundation,
+//   SwitchElectric,
+//   Arkreen,
+// } from "~/assets/companies";
 
 const Carousel: Component = () => {
   return (
-    <div class="slider">
-      <ul>
-        <li>
-          <img class="item small1" style="order: 1;" src={Arkreen} />
+    <div class="carousel">
+      <ul class="carousel__list">
+        <li class="carousel__item" data-pos="-2">
+          1
         </li>
-        <li>
-          <img class="item big1" style="order: 2;" src={SolarFoundation} />
+        <li class="carousel__item" data-pos="-1">
+          2
         </li>
-
-        <li>
-          <img class="item focus" style="order: 3;" src={SwitchElectric} />
+        <li class="carousel__item" data-pos="0">
+          3
         </li>
-
-        <li>
-          <img class="item big2" style="order: 4;" src={Arkreen} />
+        <li class="carousel__item" data-pos="1">
+          4
         </li>
-        <li>
-          <img class="item small2" style="order: 5;" src={TAS} />
+        <li class="carousel__item" data-pos="2">
+          5
         </li>
       </ul>
     </div>
