7cac2e6f17a9f7e34760ef82986a67d450d9218c
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-04-02 15:49:18 +0100

    chore: Added images

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,32 +1,53 @@
 import { Component } from "solid-js";
-// import {
-//   TAS,
-//   SolarFoundation,
-//   SwitchElectric,
-//   Arkreen,
-// } from "~/assets/companies";
+import {
+  TAS,
+  SolarFoundation,
+  SwitchElectric,
+  Arkreen,
+} from "~/assets/companies";
 
 const Carousel: Component = () => {
   return (
     <div class="carousel">
       <ul class="carousel__list">
         <li class="carousel__item" data-pos="-2">
-          1
+          <div class="image-container">
+            <img src={TAS} alt="TAS" class="carousel-image object-cover" />
+          </div>
         </li>
         <li class="carousel__item" data-pos="-1">
-          2
+          <div class="image-container">
+            <img
+              src={Arkreen}
+              alt="Arkreen"
+              class="carousel-image object-contain"
+            />
+          </div>
         </li>
         <li class="carousel__item" data-pos="0">
-          3
+          <div class="image-container"></div>
         </li>
         <li class="carousel__item" data-pos="1">
-          4
+          <div class="image-container">
+            <img
+              src={SwitchElectric}
+              alt="Switch Electric"
+              class="carousel-image object-contain"
+            />
+          </div>
         </li>
         <li class="carousel__item" data-pos="2">
-          5
+          <div class="image-container">
+            <img
+              src={SolarFoundation}
+              alt="Solar Foundation"
+              class="carousel-image object-cover"
+            />
+          </div>
         </li>
       </ul>
     </div>
   );
 };
+
 export default Carousel;
