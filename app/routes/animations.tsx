f7e8920f7ff4e697c0fdc665b37048da669c881e
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-04-25 08:23:07 +0100

    chore: Added animations page

---
+++ app/routes/animations.tsx
@@ -0,0 +1,43 @@
+import type { Route } from "./+types/animations";
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+import CircleAnimation from "~/components/CircleAnimation";
+import HorizontalScroll from "~/components/HorizontalScroll";
+import TextAnimation from "~/components/TextAnimation";
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Animations" },
+    { name: "description", content: "Welcome to Animations testing ground" },
+  ];
+}
+
+function Animations() {
+  const scroller = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      ScrollTrigger.defaults({
+        scroller: scroller.current,
+        markers: true,
+      });
+    },
+    { scope: scroller }
+  );
+
+  return (
+    <div
+      ref={scroller}
+      className={`w-full overflow-x-hidden overflow-y-auto h-[100vh]`}
+    >
+      <TextAnimation />
+      <CircleAnimation />
+      <HorizontalScroll />
+    </div>
+  );
+}
+export default Animations;
