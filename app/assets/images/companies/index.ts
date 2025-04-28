3ed9474b05f638d077608f6b335836687a37c863
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-04-28 18:26:41 +0100

    Added names to logo cards

--- app/assets/images/companies/index.ts
+++ app/assets/images/companies/index.ts
@@ -4,10 +4,10 @@ import SolarFoundation from "./solar-foundation.webp?w=320;480;768;1024;1440&as=
 import Arkreen from "./arkreen.webp?w=320;480;768;1024;1440&as=picture";
 import YourProject from "./project.webp?w=320;480;768;1024;1440&as=picture";
 import Powerledger from "./powerledger.webp?w=320;480;768;1024;1440&as=picture";
-import Logo from "./logo.webp?w=320;480;768;1024;1440&as=picture";
+import Helios from "./helios.webp?w=320;480;768;1024;1440&as=picture";
 
 const companyicons = [
-  Logo,
+  Helios,
   TAS,
   SolarFoundation,
   YourProject,
@@ -23,6 +23,6 @@ export {
   Arkreen,
   YourProject,
   Powerledger,
-  Logo,
+  Helios,
   companyicons,
 };
--- app/components/LogosCarousel.tsx
+++ app/components/LogosCarousel.tsx
@@ -1,27 +1,65 @@
-import { companyicons } from "~/assets/images/companies";
+import {
+  Arkreen,
+  Helios,
+  Powerledger,
+  SolarFoundation,
+  SwitchElectric,
+  TAS,
+  YourProject,
+} from "~/assets/images/companies";
+
+type Img = {
+  sources: {
+    [key: string]: string; // e.g. { webp: "...srcset..." }
+  };
+  img: {
+    src: string;
+    w: number;
+    h: number;
+  };
+};
+const companyLogos: { img: Img; name: string }[] = [
+  { img: Helios, name: "Helios Refi" },
+  { img: TAS, name: "TAS" },
+
+  {
+    img: SolarFoundation,
+    name: "Solar Foundations",
+  },
+  { img: YourProject, name: "" },
+  { img: SwitchElectric, name: "Switch Electric" },
+  { img: Arkreen, name: "Arkeen" },
+  {
+    img: Powerledger,
+    name: "Powerledger",
+  },
+];
 
 const LogosCarousel: React.FC = () => {
   return (
     <div
       className={`grid sm:grid-cols-7 grid-cols-1 w-full sm:h-full h-screen card-grid`}
     >
-      {companyicons.map((logo, i) => (
+      {companyLogos.map(({ img: logo, name }, i) => (
         <div
           key={i}
-          className="sm:w-full w-auto shadow-[_-1rem_0_3rem_rgba(0,0,0,0.25)] mx-auto sm:aspect-[2.5/3] aspect-[5/1] flex justify-center items-center p-3 lg:rounded-2xl rounded-xl bg-[#faf9f6]"
+          className={`space-y-1 bg-[#faf9f6] shadow-[_-1rem_0_3rem_rgba(0,0,0,0.25)] p-3 lg:rounded-2xl rounded-xl`}
         >
-          <picture>
-            {Object.entries(logo.sources).map(([type, srcset]) => (
-              <source key={type} type={`image/${type}`} srcSet={srcset} />
-            ))}
-            <img
-              src={logo.img.src}
-              width={logo.img.w}
-              height={logo.img.h}
-              alt={`Card ${i}`}
-              className={`transition-all duration-300 ease-in-out object-contain`}
-            />
-          </picture>
+          <div className="sm:w-full w-auto sm:aspect-[2.5/3] aspect-[5/1]  mx-auto flex justify-center items-center ">
+            <picture>
+              {Object.entries(logo.sources).map(([type, srcset]) => (
+                <source key={type} type={`image/${type}`} srcSet={srcset} />
+              ))}
+              <img
+                src={logo.img.src}
+                width={logo.img.w}
+                height={logo.img.h}
+                alt={`Card ${i}`}
+                className={`transition-all duration-300 ease-in-out object-contain`}
+              />
+            </picture>
+          </div>
+          <p className={`text-center`}>{name}</p>
         </div>
       ))}
     </div>
