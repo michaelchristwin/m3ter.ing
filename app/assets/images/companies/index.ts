d3e6137382a0deb3ab65b642f37e7d1e00fa251f
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-06-14 11:46:38 +0100

    chore: Fixed footer

--- app/assets/images/companies/index.ts
+++ app/assets/images/companies/index.ts
@@ -3,17 +3,17 @@ import SwitchElectric from "./switch-electric.webp?w=320;480;768;1024;1440&as=pi
 import SolarFoundation from "./solar-foundation.webp?w=320;480;768;1024;1440&as=picture";
 import Arkreen from "./arkreen.webp?w=320;480;768;1024;1440&as=picture";
 import YourProject from "./project.webp?w=320;480;768;1024;1440&as=picture";
-import Powerledger from "./powerledger.webp?w=320;480;768;1024;1440&as=picture";
 import Helios from "./helios.webp?w=320;480;768;1024;1440&as=picture";
+import AlliancePower from "./alliance_power_generation.webp?w=320;480;768;1024;1440&as=picture";
 
 const companyicons = [
   Helios,
-  TAS,
+  AlliancePower,
   SolarFoundation,
   YourProject,
   SwitchElectric,
   Arkreen,
-  Powerledger,
+  TAS,
 ];
 
 export {
@@ -22,7 +22,7 @@ export {
   SolarFoundation,
   Arkreen,
   YourProject,
-  Powerledger,
+  AlliancePower,
   Helios,
   companyicons,
 };
