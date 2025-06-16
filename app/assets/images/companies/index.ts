9ff030a812d3bffeaa404b0cca7cee0455931c0f
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-06-16 12:30:40 +0100

    chore: Added overlay for project name

--- app/assets/images/companies/index.ts
+++ app/assets/images/companies/index.ts
@@ -6,14 +6,14 @@ import YourProject from "./project.webp?w=320;480;768;1024;1440&as=picture";
 import Helios from "./helios.webp?w=320;480;768;1024;1440&as=picture";
 import AlliancePower from "./alliance_power_generation.webp?w=320;480;768;1024;1440&as=picture";
 
-const companyicons = [
-	Helios,
-	AlliancePower,
-	SolarFoundation,
-	YourProject,
-	SwitchElectric,
-	Arkreen,
-	TAS,
+const companies = [
+	{ name: "Helios", logo: Helios },
+	{ name: "Alliance Power", logo: AlliancePower },
+	{ name: "Solar Foundation", logo: SolarFoundation },
+	{ name: "Your Project", logo: YourProject },
+	{ name: "Switch Electric", logo: SwitchElectric },
+	{ name: "Arkreen", logo: Arkreen },
+	{ name: "TAS", logo: TAS },
 ];
 
 export {
@@ -24,5 +24,5 @@ export {
 	YourProject,
 	AlliancePower,
 	Helios,
-	companyicons,
+	companies,
 };
--- app/components/LogosCarousel.tsx
+++ app/components/LogosCarousel.tsx
@@ -1,4 +1,4 @@
-import { companyicons } from "~/assets/images/companies";
+import { companies } from "~/assets/images/companies";
 import { motion, type Variants } from "motion/react";
 
 const LogosCarousel: React.FC = () => {
@@ -19,8 +19,10 @@ const LogosCarousel: React.FC = () => {
 			initial={"hidden"}
 			whileInView={"show"}
 		>
-			{companyicons.map((logo, i) => {
+			{companies.map(({ logo, name }, i) => {
 				const { scale, zIndex } = getStyle(i);
+				const hoverStyle = name === "Your Project" ? { scale: 1.2 } : {};
+				const tapStyle = name === "Your Project" ? { scale: 1 } : {};
 				return (
 					<motion.div
 						key={logo.img.src}
@@ -32,8 +34,30 @@ const LogosCarousel: React.FC = () => {
 						whileInView={"show"}
 						transition={{ duration: 0.5, ease: "easeIn" }}
 						style={{ zIndex }}
-						className="md:w-full w-[180px] min-h-[180px] shadow-[_-1rem_0_3rem_rgba(0,0,0,0.25)] mx-auto sm:aspect-[2.5/3] aspect-[5/1] flex justify-center items-center p-3 lg:rounded-2xl rounded-xl bg-[#faf9f6]"
+						whileHover={hoverStyle}
+						whileTap={tapStyle}
+						onClick={() => {
+							if (name === "Your Project") {
+								window.open(
+									"https://form.typeform.com/to/d58WsifL",
+									"_blank",
+									"noopener,noreferrer",
+								);
+							}
+						}}
+						className={`md:w-full w-[180px] min-h-[180px] shadow-[_-1rem_0_3rem_rgba(0,0,0,0.25)] mx-auto sm:aspect-[2.5/3] aspect-[5/1] flex justify-center items-center p-3 lg:rounded-2xl rounded-xl bg-[#faf9f6] relative group ${
+							name === "Your Project" && "cursor-pointer"
+						}`}
 					>
+						{/* Hover Overlay */}
+						{name !== "Your Project" && (
+							<div className="absolute inset-0 top-0 left-0 h-full w-full rounded-xl bg-gray-600/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out flex items-center justify-center">
+								<h3 className="text-white text-lg font-semibold text-center px-4">
+									{name}
+								</h3>
+							</div>
+						)}
+
 						<picture>
 							{Object.entries(logo.sources).map(([type, srcset]) => (
 								<source key={type} type={`image/${type}`} srcSet={srcset} />
@@ -54,6 +78,7 @@ const LogosCarousel: React.FC = () => {
 		</motion.div>
 	);
 };
+
 export default LogosCarousel;
 
 const getStyle = (i: number) => {
