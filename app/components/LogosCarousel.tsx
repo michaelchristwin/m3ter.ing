49e067085048d4d7bb5992dfd1cd10eb152b8f32
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-07-23 15:06:27 +0100

    Added crawable link for typeform

--- app/components/LogosCarousel.tsx
+++ app/components/LogosCarousel.tsx
@@ -1,5 +1,7 @@
 import { companies } from "~/assets/images/companies";
 import { motion, type Variants } from "motion/react";
+import { Link } from "react-router";
+import { Fragment } from "react/jsx-runtime";
 
 const LogosCarousel: React.FC = () => {
   const container: Variants = {
@@ -23,33 +25,23 @@ const LogosCarousel: React.FC = () => {
         const { scale, zIndex } = getStyle(i);
         const hoverStyle = name === "Your Project" ? { scale: 1.2 } : {};
         const tapStyle = name === "Your Project" ? { scale: 1 } : {};
-        return (
+        const content = (
           <motion.div
             key={logo.img.src}
             variants={{
               hidden: { opacity: 0, y: 50, scale },
               show: { opacity: 1, y: 0, scale },
             }}
-            initial={"hidden"}
-            whileInView={"show"}
+            initial="hidden"
+            whileInView="show"
             transition={{ duration: 0.5, ease: "easeIn" }}
             style={{ zIndex }}
             whileHover={hoverStyle}
             whileTap={tapStyle}
-            onClick={() => {
-              if (name === "Your Project") {
-                window.open(
-                  "https://form.typeform.com/to/d58WsifL",
-                  "_blank",
-                  "noopener,noreferrer"
-                );
-              }
-            }}
             className={`md:w-full w-[180px] min-h-[180px] shadow-[_-1rem_0_3rem_rgba(0,0,0,0.25)] mx-auto sm:aspect-[2.5/3] aspect-[5/1] flex justify-center items-center p-3 lg:rounded-2xl rounded-xl bg-[#faf9f6] relative group ${
               name === "Your Project" && "cursor-pointer"
             }`}
           >
-            {/* Hover Overlay */}
             {name !== "Your Project" && (
               <div className="absolute inset-0 top-0 left-0 h-full w-full rounded-xl bg-gray-600/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out flex items-center justify-center">
                 <h3 className="text-white text-lg font-semibold text-center px-4">
@@ -69,12 +61,22 @@ const LogosCarousel: React.FC = () => {
                 width={logo.img.w}
                 height={logo.img.h}
                 alt={`Card ${i}`}
-                className={
-                  "transition-all duration-300 ease-in-out object-cover"
-                }
+                className="transition-all duration-300 ease-in-out object-cover"
               />
             </picture>
           </motion.div>
+        );
+        return name === "Your Project" ? (
+          <Link
+            to="https://form.typeform.com/to/d58WsifL"
+            target="_blank"
+            key={name}
+            rel="noopener noreferrer"
+          >
+            {content}
+          </Link>
+        ) : (
+          <Fragment key={name}>{content}</Fragment>
         );
       })}
     </motion.div>
